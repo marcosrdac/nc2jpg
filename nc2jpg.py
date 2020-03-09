76e2a920def110a75f0061974113e60d0cfbcba3
Marcos Conceição <marcosrdac@gmail.com>
2020-03-09 17:05:50 -0300

    first commit

---
+++ nc2jpg.py
@@ -0,0 +1,43 @@
+import numpy as np
+import netCDF4 as nc
+from PIL import Image
+from os.path import splitext
+from sys import argv
+
+
+def choose_variable(ncf):
+    '''
+    Show all variables of an open NetCDF file.
+    '''
+    variables = list(ncf.variables.keys())
+    print('Choose a variable to be converted:')
+    for i, variable in enumerate(variables):
+        print(f'\t{i+1}\t{variable}')
+    variable_number = int(input('Variable number: '))-1
+    variable = variables[variable_number]
+    print()
+    return(variable)
+
+
+assert len(argv)>1
+filename=argv[1]
+ncf = nc.Dataset(filename)
+
+if len(argv)<3:
+    variable = choose_variable(ncf)
+else:
+    variable=argv[2]
+if len(argv)<4:
+    quality = input('Quality of compression (0-100): ')
+    quality = int(quality)
+else:
+    variable=argv[3]
+
+img = ncf[variable]
+maxval = np.max(img)
+minval = np.min(img)
+img = 255 * (img - minval)/(maxval - minval)
+img = Image.fromarray(img.astype(np.uint8))
+
+outfilename = splitext(filename)[0]
+img.save(f'{outfilename}.jpg', quality=quality)
