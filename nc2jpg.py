6b7e8a726ad3c30841fab8ec6aa79385b308c545
Marcos Conceição <marcosrdac@gmail.com>
2020-03-13 15:57:18 -0300

    updates on code

--- nc2jpg.py
+++ nc2jpg.py
@@ -1,8 +1,10 @@
 import numpy as np
 import netCDF4 as nc
 from PIL import Image
-from os.path import splitext
+from os.path import splitext, isdir, join
 from sys import argv
+
+FALLBACK_EXT='.jpg'
 
 
 def choose_variable(ncf):
@@ -18,25 +20,45 @@ def choose_variable(ncf):
     print()
     return(variable)
 
-
-assert len(argv)>1
-filename=argv[1]
-if len(argv)<3:
+# reading inputfrom user
+assert len(argv) > 1
+filepath = argv[1]  # always
+options  = argv[2:]  # 0 is python
+# opening NetCDF file
+ncf = nc.Dataset(filepath)
+while len(options) > 1:
+    if options[0] == '-v':
+        variable = options[1]
+    if options[0] == '-o':
+        outfilepath = options[1]
+    if options[0] == '-q':
+        quality = int(options[1])
+    del(options[:2])
+if not 'variable' in globals():
     variable = choose_variable(ncf)
-else:
-    variable=argv[2]
-if len(argv)<4:
+if not 'quality' in globals():
     quality = input('Quality of compression (0-100): ')
     quality = int(quality)
-else:
-    variable=argv[3]
+if not 'outfilepath' in globals():
+    outfilepath = splitext(filepath)[0] + FALLBACK_EXT
 
-ncf = nc.Dataset(filename)
+# checking if output is a directory or a filepath
+name = splitext(filepath)[0]
+ext  = splitext(outfilepath)[1]
+outext  = ext if ext else FALLBACK_EXT
+if isdir(outfilepath):
+    outfilepath = join(outfilepath, name+outext)
+
+print(outfilepath)
+
+# getting NetCDF variable
 img = ncf[variable]
+
+# scaling variable to fit a RGB image
 maxval = np.max(img)
 minval = np.min(img)
 img = 255 * (img - minval)/(maxval - minval)
 img = Image.fromarray(img.astype(np.uint8))
 
-outfilename = splitext(filename)[0]
-img.save(f'{outfilename}.jpg', quality=quality)
+# saving file
+img.save(outfilepath, quality=quality)
